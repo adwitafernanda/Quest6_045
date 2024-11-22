84e6df3e2798480ee3f8fa49922844789d47b9be
adwitafernanda <adwitafernanda4@gmail.com>
2024-11-22 15:40:08 +0700

    tambah fungsi untuk mengatur mata kuliah yang dipilih

--- app/src/main/java/com/example/navigationcompose/ui/view/viewmodel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/example/navigationcompose/ui/view/viewmodel/RencanaStudyViewModel.kt
@@ -5,8 +5,14 @@ import com.example.navigationcompose.model.RencanaStudi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class RencanaStudyViewModel : ViewModel() {
     private val _krsState = MutableStateFlow(RencanaStudi())
     val krsStateUI: StateFlow<RencanaStudi> = _krsState.asStateFlow()
+
+    fun setMataKuliah(mkPilihan: String) {
+        _krsState.update { stateMK -> stateMK.copy (namaMK = mkPilihan) }
+    }
+
 }
