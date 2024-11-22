2d17d3b22738b9e7fd87ef8346dc5773fe76b477
adwitafernanda <adwitafernanda4@gmail.com>
2024-11-22 16:04:26 +0700

    menambah email pada data class mahasiswa

--- app/src/main/java/com/example/navigationcompose/model/Mahasiswa.kt
+++ app/src/main/java/com/example/navigationcompose/model/Mahasiswa.kt
@@ -1,9 +1,7 @@
 package com.example.navigationcompose.model
 
-class Mahasiswa {
-    val nim: String ="",
-    val nama: String ="",
-    val email: String ="",
-
-
-}
+data class Mahasiswa(
+    val nim: String = "",
+    val nama: String = "",
+    val email: String = ""
+)
