4c11ca6a168242492339c1ffb69088076f5e097a
adwitafernanda <adwitafernanda4@gmail.com>
2024-11-22 15:34:29 +0700

    tambah state untuk mengelola data KRS di UI

--- app/src/main/java/com/example/navigationcompose/ui/view/viewmodel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/example/navigationcompose/ui/view/viewmodel/RencanaStudyViewModel.kt
@@ -3,7 +3,10 @@ package com.example.navigationcompose.ui.view.viewmodel
 import androidx.lifecycle.ViewModel
 import com.example.navigationcompose.model.RencanaStudi
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class RencanaStudyViewModel : ViewModel() {
     private val _krsState = MutableStateFlow(RencanaStudi())
+    val krsStateUI: StateFlow<RencanaStudi> = _krsState.asStateFlow()
 }
